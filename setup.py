718843ba1af72bb728dd09366140c4fdf2e51d5c
Lajos Santa <santa.lajos@coldline.hu>
2015-04-03 22:34:38 +0200

    Missing files from prev commit

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name = "Plutonium",
     version = '0.4',
     install_requires = [
-        "SQLAlchemy >= 0.9.8"
+        "SQLAlchemy >= 0.9.8",
+        "alembic == 0.7.4",
     ],
     packages = find_packages(),
+    include_package_data = True,
     scripts = [
         'bin/plutonium',
         'bin/plutonium_starter',
