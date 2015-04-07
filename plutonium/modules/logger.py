4f6393d532b26fac5c8f6c46cdccce74e90d084f
Lajos Santa <santa.lajos@coldline.hu>
2015-04-07 23:31:22 +0200

    fix: Redirect error prints to the logfile

--- plutonium/modules/logger.py
+++ plutonium/modules/logger.py
@@ -2,16 +2,6 @@ import logging
 import logging.config
 
 import json
-import sys
-
-class StdWriter(object):
-    def __init__(self, logger, level):
-        self.logger = logger
-        self.level = level
-
-    def write(self, text):
-        self.logger.log(self.level, text)
-
 
 def load_logger(config_file_name):
     with open(config_file_name) as f:
@@ -19,25 +9,16 @@ def load_logger(config_file_name):
 
 def get_logger(name, module = None):
     logger = logging.getLogger('%s.%s' % (module, name) if module else name)
-
-    #sys.stderr = StdWriter(logger, logging.ERROR)
-    #sys.stdout = StdWriter(logger, logging.WARNING)
-
     return logger
 
-"""
-def get_logger(config_file_name, name):
-    with open(config_file_name) as f:
-        content = f.read()
-    config_data = json.loads(content)
+class StdWriter(object):
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+        self.msg = ''
 
-    logging.config.dictConfig(config_data)
-
-    logger = logging.getLogger(name)
-
-    sys.stderr = StdWriter(logger, logging.ERROR)
-    sys.stdout = StdWriter(logger, logging.WARNING)
-
-    return logger
-
-"""
+    def write(self, text):
+        if text.startswith('\n'):
+            self.logger.log(self.level, self.msg)
+            self.msg = ''
+        self.msg += text
